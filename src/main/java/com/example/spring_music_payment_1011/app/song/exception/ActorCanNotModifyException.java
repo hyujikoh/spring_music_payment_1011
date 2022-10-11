0e45dce6c0f3b2803612f7da327b374d6ffd715c
hyujikoh <dvum0045@gmail.com>
2022-10-11 14:52:24 +0900

    음원 서비스 테스트

---
+++ src/main/java/com/example/spring_music_payment_1011/app/song/exception/ActorCanNotModifyException.java
@@ -0,0 +1,4 @@
+package com.example.spring_music_payment_1011.app.song.exception;
+
+public class ActorCanNotModifyException extends RuntimeException {
+}
---
+++ src/main/java/com/example/spring_music_payment_1011/app/song/form/SongForm.java
@@ -0,0 +1,13 @@
+package com.example.spring_music_payment_1011.app.song.form;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class SongForm {
+    @NotEmpty
+    private String subject;
+    @NotEmpty
+    private String content;
+}
